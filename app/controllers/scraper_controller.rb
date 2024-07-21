d5c9d404d9f111d10414389863c5c2a7939c6fd9
Jay <jayeshpatil8041@gmail.com>
2024-07-22 04:12:33 +0530

    add code to handle params and code refactor

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -5,13 +5,11 @@ class ScraperController < ApplicationController
   BASE_URL = 'https://www.ycombinator.com/companies'
   MAX_RETRIES = 2
 
-  def initialize
-    @companies = []
-    @driver = driver_setup
-    @wait = Selenium::WebDriver::Wait.new(timeout: 10) # Set an explicit wait timeout
-  end
+  before_action :initialize_scraper
 
   def index
+    @n = params[:n].present? && params[:n].to_i > 0 ? params[:n].to_i : 60
+
     load_page
     collect_companies
 
@@ -21,6 +19,12 @@ class ScraperController < ApplicationController
   end
 
   private
+
+  def initialize_scraper
+    @companies = []
+    @driver = driver_setup
+    @wait = Selenium::WebDriver::Wait.new(timeout: 10) # Set an explicit wait timeout
+  end
   
   def driver_setup
     options = Selenium::WebDriver::Chrome::Options.new
@@ -34,24 +38,34 @@ class ScraperController < ApplicationController
   end
 
   def collect_companies
+    retry_count = 0
     begin
-      # Find all company cards
-      company_cards = @driver.find_elements(css: '._company_86jzd_338')
+      company_cards = []
+      while @companies.size < @n
+        old_size = company_cards.size
+        @driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-      company_cards.each_with_index do |company_card, index|
-        company = extract_company_info(company_card)
-        details_link = company_card.attribute('href')
-        fetch_company_details(details_link, company)
+        @wait.until do
+          company_cards = @driver.find_elements(css: '._company_86jzd_338')
+          company_cards.size > old_size
+        end
 
-        @companies << company
+        company_cards.each_with_index do |company_card, index|
+          break if @companies.size >= @n
+          company = extract_company_info(company_card)
+          details_link = company_card.attribute('href')
+          fetch_company_details(details_link, company)
 
-        # Close the details tab and switch back to the main tab
-        @driver.close
-        @driver.switch_to.window(@driver.window_handles.first)
+          @companies << company
 
-        # Re-fetch the company cards to ensure the references are valid
-        # Limit to re-fetch only up to the current index to avoid re-processing
-        company_cards = @driver.find_elements(css: '._company_86jzd_338').first(index + 1)
+          # Close the details tab and switch back to the main tab
+          @driver.close
+          @driver.switch_to.window(@driver.window_handles.first)
+
+          # Re-fetch the company cards to ensure the references are valid
+          # Limit to re-fetch only up to the current index to avoid re-processing
+          company_cards = @driver.find_elements(css: '._company_86jzd_338').first(index + 1)
+        end
       end
 
     rescue Selenium::WebDriver::Error::NoSuchElementError => e
@@ -79,20 +93,25 @@ class ScraperController < ApplicationController
   end
 
   def fetch_company_details(url, company)
-    # Open the company details page in a new tab
-    @driver.execute_script("window.open(arguments[0], '_blank');", url)
-    @driver.switch_to.window(@driver.window_handles.last)
-    @wait.until { @driver.find_element(css: '.leading-none.text-linkColor a') }
+    retry_count = 0
+    begin
+      # Open the company details page in a new tab
+      @driver.execute_script("window.open(arguments[0], '_blank');", url)
+      @driver.switch_to.window(@driver.window_handles.last)
+      @wait.until { @driver.find_element(css: '.leading-none.text-linkColor a') }
 
-    page = Nokogiri::HTML(@driver.page_source)
+      page = Nokogiri::HTML(@driver.page_source)
 
-    company[:company_website] = page.at_css('.leading-none.text-linkColor a')['href']
-    company[:founders] = page.css('div.leading-snug').map { |ele| ele.text.strip }
-    company[:linkedin_profiles] = []
-    page.css('div.leading-snug a[title="LinkedIn profile"]').each do |link|
-      company[:linkedin_profiles] << link['href']
+      company[:company_website] = page.at_css('.leading-none.text-linkColor a')['href']
+      company[:founders] = page.css('div.leading-snug').map { |ele| ele.text.strip }
+      company[:linkedin_profiles] = []
+      page.css('div.leading-snug a[title="LinkedIn profile"]').each do |link|
+        company[:linkedin_profiles] << link['href']
+      end
+    rescue Selenium::WebDriver::Error::StaleElementReferenceError => e
+      Rails.logger.error("Stale element reference: #{e.message}")
+      retry_count += 1
+      retry if retry_count < MAX_RETRIES
     end
-  rescue Selenium::WebDriver::Error::StaleElementReferenceError => e
-    Rails.logger.error("Stale element reference: #{e.message}")
   end
 end
